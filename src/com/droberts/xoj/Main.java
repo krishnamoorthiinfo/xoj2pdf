1e1a71c1fe69ce2f3cbc514d4bf549d0de1b2444
David Roberts <droberts@ombsoft.com>
2011-05-07 21:23:41 +0100

    Can now convert entire directory trees by specifying -r as the third parameter

--- src/com/droberts/xoj/Main.java
+++ src/com/droberts/xoj/Main.java
@@ -35,7 +35,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		// Check args
-		if (args.length != 2)
+		if (args.length < 2)
 		{
 			System.out.println("Expected two parameters: Input directory to convert and converted destination directory");
 			return;
@@ -61,68 +61,118 @@ public class Main
 			return;
 		}
 		
+		// Recursion on or off?
+		if (args.length == 3)
+		{
+			// Recursion specified?
+			if (args[2].equals("-r"))
+			{
+				// Allow recursive conversion
+				recurseOnDirectories(inputFolder, outputFolder, true);
+				
+			} else {
+				
+				// Don't perform recursive conversion
+				recurseOnDirectories(inputFolder, outputFolder, false);
+			}
+			
+		} else {
+			
+			// Don't perform recursive conversion
+			recurseOnDirectories(inputFolder, outputFolder, false);
+		}
 		
+		System.out.println("xoj2pdf finished");
+	}
+	
+	/**
+	 * Recurse into a given folder
+	 * @param directory directory we're in
+	 * @param outputFolder directory to output to
+	 * @param allowRecursion whether to further recurse (called with false converts with no recursion)
+	 */
+	private static void recurseOnDirectories(File directory, File outputFolder, boolean allowRecursion)
+	{
 		// Right, let's go to work...
 		
 		// Create a loader for XOJs
 		Loader loader = new Loader();
 		
 		// List files in input dir
-		File[] xojs = inputFolder.listFiles();
+		File[] xojs = directory.listFiles();
 
 		// Only want XOJs
 		FileFilter fileFilter = new FileFilter() {
 		    public boolean accept(File file) {
-		        return file.getName().toLowerCase().endsWith(".xoj");
+		        return file.getName().toLowerCase().endsWith(".xoj") || file.isDirectory();
 		    }
 		};
 		
 		// List all XOJs
-		xojs = inputFolder.listFiles(fileFilter);
+		xojs = directory.listFiles(fileFilter);
+	
 		
 		// Iterate each XOJ
 		for (int i = 0; i < xojs.length; i++)
 		{
-			try 
+			// If it's a file, convert it
+			if (xojs[i].isFile())
 			{
-				// Load the .xoj
-				Document xojDocument = loader.load(xojs[i].getAbsolutePath());
+				try 
+				{
+					// Load the .xoj
+					Document xojDocument = loader.load(xojs[i].getAbsolutePath());
+					
+					// Create output name
+					String outputName = outputFolder.getAbsolutePath() + "/" + xojs[i].getName().replace(".xoj",".pdf");
+					
+					Progress.onStartFile(xojs[i].getAbsolutePath(), outputName);
+					
+					// Prepare to generate pages
+					PageGenerator pageGen = new PageGenerator(xojDocument);
 				
-				// Create output name
-				String outputName = outputFolder.getAbsolutePath() + "/" + xojs[i].getName().replace(".xoj",".pdf");
-				
-				Progress.onStartFile(xojs[i].getAbsolutePath(), outputName);
-				
-				// Prepare to generate pages
-				PageGenerator pageGen = new PageGenerator(xojDocument);
+					// Prepare to export with the given renderer
+					Exporter export = new Exporter(new PdfRenderer(outputName));
+					
+					// Render pages on the page generator's output
+					export.renderPages(pageGen.paginate());
+					
+				} 
+				catch (ParserConfigurationException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+				catch (SAXException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+				catch (FileNotFoundException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			
-				// Prepare to export with the given renderer
-				Exporter export = new Exporter(new PdfRenderer(outputName));
+			} else {
 				
-				// Render pages on the page generator's output
-				export.renderPages(pageGen.paginate());
-				
-			} 
-			catch (ParserConfigurationException e) 
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-			catch (SAXException e) 
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-			catch (FileNotFoundException e) 
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				// If we're allowing recursion...
+				if (allowRecursion)
+				{
+					// Plug this onto our current output
+					File newRecurseLocation = new File(outputFolder.getAbsolutePath() + "/" + xojs[i].getName() );
+					newRecurseLocation.mkdirs();
+					
+					System.out.println("Recursing into " + xojs[i] +" outputting to: " + newRecurseLocation.getAbsolutePath() );
+					
+					// Otherwise, recurse
+					recurseOnDirectories(xojs[i], newRecurseLocation, allowRecursion);
+				} 
 			}
 		}
-		
-		
-		
 	}
+	
+
 	
 	
 }
